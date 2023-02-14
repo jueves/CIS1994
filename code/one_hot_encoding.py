f1727955add8b58a8fd857aaeb77f989404146ca
Luis Cobiella Hernández <alu0100358797@ull.edu.es>
2023-02-14 18:41:08 +0000

    Add random forest classification

--- code/one_hot_encoding.py
+++ code/one_hot_encoding.py
@@ -5,20 +5,30 @@ def one_hot_encoding(data, target):
     '''
     One hot encodes dataframe
     '''
-    categorical_columns = [col for col in data.columns if data[col].dtype.name == "category"]
-
-    encoder = OneHotEncoder()
-
-    one_hot_encoded = pd.DataFrame(encoder.fit_transform(data[categorical_columns]).toarray(),
-                                                         columns=encoder.get_feature_names_out(categorical_columns))
-
-    # Drop original categorical columns and concatenate one-hot encoded columns
-    data = data.drop(categorical_columns, axis=1)
-    data = pd.concat([data, one_hot_encoded], axis=1)
 
     # Split data into features (X) and labels (y)
     X = data.drop(target, axis=1)
     y = data[target]
+    
+
+
+    categorical_columns = [col for col in X.columns if X[col].dtype.name == "category"]
+
+    encoder = OneHotEncoder()
+
+    one_hot_encoded = pd.DataFrame(encoder.fit_transform(X[categorical_columns]).toarray(),
+                                                         columns=encoder.get_feature_names_out(categorical_columns))
+
+    # Drop original categorical columns and concatenate one-hot encoded columns
+    X = X.drop(categorical_columns, axis=1)
+ 
+    # Reset indexes to avoid row duplicates
+    X = X.reset_index()
+    one_hot_encoded = one_hot_encoded.reset_index()
+
+    # Concatenate
+    X = pd.concat([X, one_hot_encoded], axis=1)
+    
 
     return(X, y)
 
